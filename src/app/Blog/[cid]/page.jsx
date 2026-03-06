a118ddacd39f0b273c2ff2356149bdbecfc1b6f3
R Bharathi Kumar <llcirclebharathi@gmail.com>
2026-03-06 20:34:44 +0530

    third commit

--- src/app/Blog/[cid]/page.jsx
+++ src/app/Blog/[cid]/page.jsx
@@ -10,7 +10,7 @@ export default function BlogPage() {
   const { cid } = resolvedParams;
   return (
     <div
-      className={` min-h-screen max-h-screen md:overflow-y-auto md:[&::-webkit-scrollbar]:w-2 md:[&::-webkit-scrollbar-thumb]:bg-[#00ADB5] md:[&::-webkit-scrollbar-thumb]:rounded-full md:[&::-webkit-scrollbar-track]:my-4 transition-all duration-300 ${DarkMode ? "bg-[#1A1A1B] md:[&::-webkit-scrollbar-track]:bg-[#1A1A1B]" : "bg-[#F9F9F7] md:[&::-webkit-scrollbar-track]:bg-[#F9F9F7]"}`}
+      className={` min-h-screen md:max-h-screen md:overflow-y-auto md:[&::-webkit-scrollbar]:w-2 md:[&::-webkit-scrollbar-thumb]:bg-[#00ADB5] md:[&::-webkit-scrollbar-thumb]:rounded-full md:[&::-webkit-scrollbar-track]:my-4 transition-all duration-300 ${DarkMode ? "bg-[#1A1A1B] md:[&::-webkit-scrollbar-track]:bg-[#1A1A1B]" : "bg-[#F9F9F7] md:[&::-webkit-scrollbar-track]:bg-[#F9F9F7]"}`}
     >
       <BlogPageDesign DarkMode={DarkMode} setDarkMode={setDarkMode} cid={cid} />
     </div>
